067ce99bece904d902fcb46a2aad52b572a2d6a9
zzh <108445240+X0X0X00@users.noreply.github.com>
2024-07-24 17:45:05 +0800

    Add files via upload

---
+++ Project03/Edge.java
@@ -0,0 +1,18 @@
+/// Zhenhao Zhang zzh133
+
+
+
+public class Edge {
+	public final String id;
+	public final String node1;
+	public final String node2;
+	public final double weight;
+
+	public Edge(String id, String node1, String node2, double weight) {
+		this.id = id;
+		this.node1 = node1;
+		this.node2 = node2;
+		this.weight = weight;
+	}
+
+}
