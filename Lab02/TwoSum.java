d1355ecc4c259cbe747c916b9fbe524e25476b6a
zzh <108445240+X0X0X00@users.noreply.github.com>
2024-07-24 17:24:02 +0800

    Add files via upload

---
+++ Lab02/TwoSum.java
@@ -0,0 +1,98 @@
+/******************************************************************************
+ *  Compilation:  javac Data_TwoSum.java
+ *  Execution:    java Data_TwoSum input.txt
+ *  Dependencies: StdOut.java In.java Stopwatch.java
+ *  Data files:   http://algs4.cs.princeton.edu/14analysis/1Kints.txt
+ *                http://algs4.cs.princeton.edu/14analysis/2Kints.txt
+ *                http://algs4.cs.princeton.edu/14analysis/4Kints.txt
+ *                http://algs4.cs.princeton.edu/14analysis/8Kints.txt
+ *                http://algs4.cs.princeton.edu/14analysis/16Kints.txt
+ *                http://algs4.cs.princeton.edu/14analysis/32Kints.txt
+ *                http://algs4.cs.princeton.edu/14analysis/1Mints.txt
+ *
+ *  A program with n^2 running time. Reads n integers
+ *  and counts the number of pairs that sum to exactly 0.
+ *
+ *
+ *  Limitations
+ *  -----------
+ *     - we ignore integer overflow
+ *
+ *
+ *  % java Data_TwoSum 2Kints.txt
+ *  2
+ *
+ *  % java Data_TwoSum 1Kints.txt
+ *  1
+ *
+ *  % java Data_TwoSum 2Kints.txt
+ *  2
+ *
+ *  % java Data_TwoSum 4Kints.txt
+ *  3
+ *
+ *  % java Data_TwoSum 8Kints.txt
+ *  19
+ *
+ *  % java Data_TwoSum 16Kints.txt
+ *  66
+ *
+ *  % java Data_TwoSum 32Kints.txt
+ *  273
+ *
+ ******************************************************************************/
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class TwoSum {
+
+    // print distinct pairs (i, j) such that a[i] + a[j]  = 0
+    public static void printAll(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                if (a[i] + a[j] == 0) {
+                    StdOut.println(a[i] + " " + a[j]);
+                }
+            }
+        }
+    } 
+
+    // return number of distinct pairs (i, j) such that a[i] + a[j] = 0
+    public static int count(int[] a) {
+        int n = a.length; // O(1)
+        int count = 0;
+        // outer loop: i = 1 - n
+        for (int i = 0; i < n; i++) {
+            // inter loop: from i+1 to n
+            for (int j = i+1; j < n; j++) {
+                if (a[i] + a[j] == 0) {
+                    count++;
+                }
+            }
+        }
+        // total: 1+2+3+ ... +n-1 = O(N^2)
+        return count;
+    } 
+
+ /**
+     * 
+     * counts the number of pairs sum to exactly zero; prints out the time to perform
+     * the computation along with other information.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args)  { 
+        In in = new In(args[0]);
+        int[] a = in.readAllInts();
+        Stopwatch timer = new Stopwatch();
+        int count = count(a);
+        double time = timer.elapsedTime();
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+          //TODO: Replace with your own netid
+        String netID = "zzh133";
+          StdOut.printf("%7d %7.1f   %s  %s  %s\n", count, time, timeStamp, netID, args[0]); // 为了作图改为 %7.6f
+    } 
+} 
+
+
